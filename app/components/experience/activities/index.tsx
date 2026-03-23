2456f8fd235e4e64f85a88b10a1e21d40b927011
kali <kali@kali.kali>
2026-03-23 12:37:01 +0530

    Adjust camera Y position for mobile

--- app/components/experience/activities/index.tsx
+++ app/components/experience/activities/index.tsx
@@ -137,7 +137,7 @@ const Activities = () => {
     if (isActive) {
       if (isMobile) {
         gsap.to(camera.position, {
-          y: -50,
+          y: -10,
           x: 0,
           z: 11.5,
           duration: 1,
