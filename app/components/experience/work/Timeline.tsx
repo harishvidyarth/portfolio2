e33c915f98046e335fea931169311aa05f0f440f
kali <kali@kali.kali>
2026-03-28 08:54:17 +0530

    Move Rajalakshmi year up to 0.4

--- app/components/experience/work/Timeline.tsx
+++ app/components/experience/work/Timeline.tsx
@@ -45,7 +45,7 @@ const TimelinePoint = ({ point, diff }: { point: WorkTimelinePoint, diff: number
       </Box>
       <group>
         <group position={getPoint}>
-          <Text {...textProps} fontSize={0.3} position={[-diff / 2, 0.25, 0]}>
+          <Text {...textProps} fontSize={0.3} position={[-diff / 2, point.title.includes('Rajalakshmi') ? 0.4 : 0.25, 0]}>
             {point.year}
           </Text>
           <group position={[0, -0.5, 0]}>
